281ce2c22bacac6d877bbd97f2d492569a3d291d
osls <balipastika@gmail.com>
2023-04-19 00:55:12 +0800

    refactor

--- lib/shareds/general_helper/firebase_key_constant.dart
+++ lib/shareds/general_helper/firebase_key_constant.dart
@@ -1,6 +1,7 @@
 
 class FirebaseKeyConstant {
   static String collectionUserKey = "user_store";
+  static String collectionConversationKey = "conversations";
   static String collectionTutorKey = "teacher_role";
 }
 
